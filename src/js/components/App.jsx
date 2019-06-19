efd13084ff9276e531e743b748f0661db42f2741
vvorkcloudtechnologies <tariqali99.ta@gmail.com>
2019-06-19 12:38:22 +0500

    anees branch initial commit

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="app">this is app</div>;
+    return <div className="app">this is app for anees</div>;
   }
 }
 export default App;
